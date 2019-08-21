9e5166ab35db2b143755f19d0dacdf1fb7e110ef
LegibleCypress6 <xander_2006@icloud.com>
2019-08-21 16:52:27 -0400

    Finished both practice teleops and libraries!!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/evanTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/evanTeleop.java
@@ -1,18 +1,22 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 /**
  * Created by dcrenshaw on 4/7/19.
  * Clasp is a basic, bare-bones teleop designed to run on top of Anvil.
  */
-
+@TeleOp(name="Evan", group="PushBot")
+//@Disabled
 public class evanTeleop extends OpMode {
 
     private evanForge robot;
+    @Override
     public void init() {
         robot = new evanForge(hardwareMap, evanForge.Drivetrain.EVAN, telemetry);
     }
+    @Override
     public void loop() {
         if (gamepad1.left_stick_y > 0) {
             robot.forward(-1);
