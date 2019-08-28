dbcff1e313374b8cd0bc4fbe86bdef1008d33ec6
evan-blip <evan.wieberg@students.thalesacademy.org>
2019-08-28 17:13:36 -0400

    I think I fixed my turns again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/evansauton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/evansauton.java
@@ -52,15 +52,15 @@ public class evansauton extends LinearOpMode {
         robot.forward(1);
         sleep(1000);
         robot.right(1);
-        sleep(500);
+        sleep(750);
         robot.forward(1);
         sleep(1000);
         robot.right(1);
-        sleep(500);
+        sleep(750);
         robot.forward(1);
         sleep(1000);
         robot.right(1);
-        sleep(500);
+        sleep(750);
         robot.forward(1);
         sleep(1000);
         robot.stop();
